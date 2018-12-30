09ee97e30963e0286f6bf62a34f19ed9def9be1d
DrishtiDas <dasdrishtid@gmail.com>
2018-12-30 14:12:58 +0530

    Initial commit

---
+++ db/paysystem.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 06, 2017 at 03:30 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `paysystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `id` int(255) NOT NULL,
+  `branch` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `detail` text NOT NULL,
+  `delete_status` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`id`, `branch`, `address`, `detail`, `delete_status`) VALUES
+(1, 'Swindon Branch', '1788 Traction Street', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', '0'),
+(2, 'Bristol Bedminster', '4705 School Street', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', '0'),
+(3, 'Kingswood School', '3386 Rebecca Street', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fees_transaction`
+--
+
+CREATE TABLE `fees_transaction` (
+  `id` int(255) NOT NULL,
+  `stdid` varchar(255) NOT NULL,
+  `paid` int(255) NOT NULL,
+  `submitdate` datetime NOT NULL,
+  `transcation_remark` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fees_transaction`
+--
+
+INSERT INTO `fees_transaction` (`id`, `stdid`, `paid`, `submitdate`, `transcation_remark`) VALUES
+(1, '1', 100, '2017-11-01 00:00:00', ''),
+(2, '2', 150, '2017-10-05 00:00:00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(255) NOT NULL,
+  `emailid` varchar(255) NOT NULL,
+  `sname` varchar(255) NOT NULL,
+  `joindate` datetime NOT NULL,
+  `about` text NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `fees` int(255) NOT NULL,
+  `branch` varchar(255) NOT NULL,
+  `balance` int(255) NOT NULL,
+  `delete_status` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `emailid`, `sname`, `joindate`, `about`, `contact`, `fees`, `branch`, `balance`, `delete_status`) VALUES
+(1, 'PaigeDoherty@gmail.com', 'Paige Doherty', '2017-11-01 00:00:00', 'Loreim Ipsum', '07785383', 100, '3', 0, '0'),
+(2, 'CharlesMiles@gmail.com', 'Charles Miles', '2017-10-05 00:00:00', 'Hurry while stocks last:  Cleeve School blazers have all been reduced in price. ', '07748278', 150, '1', 0, '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `emailid` varchar(255) NOT NULL,
+  `lastlogin` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `emailid`, `lastlogin`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Lewa', 'lewa@gmail.com', '0000-00-00 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fees_transaction`
+--
+ALTER TABLE `fees_transaction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branch`
+--
+ALTER TABLE `branch`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `fees_transaction`
+--
+ALTER TABLE `fees_transaction`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
